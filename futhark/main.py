5d15ea402a9b53f8bfe928fd387cc3384c92f620
Sipondo <tt_robroek@hotmail.com>
2018-10-15 10:33:27 +0200

    treesolver typo

--- futhark/main.py
+++ futhark/main.py
@@ -2,9 +2,9 @@ import numpy as np
 import sys
 sys.path.append("")
 sys.path.append("futhark")
-from treeresolver import treeresolver
+from treesolver import treesolver
 
 tree = treesolver()
-res = treeresolver.pred1(0,1)
+res = tree.pred1(0,1)
 print("And the result itself:")
 print(res)
